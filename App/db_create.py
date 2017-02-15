28738dd1cd867bab6573db226a8b26e362d97cb3
Anssi Vaakanainen <anssi.vaakanainen@gmail.com>
2017-02-15 05:05:58 +0200

    hjstory

--- App/db_create.py
+++ App/db_create.py
@@ -5202,6 +5202,19 @@ Sarjakuva(
 	next="a|button|i.fa-chevron-right",
 ),
 
+Sarjakuva(
+	"HJ Story", 
+	"hjstory",
+
+	"http://www.hj-story.com/", 
+	"http://www.hj-story.com/story-begins/",
+	test="http://www.hj-story.com/love-is-music-to-my-ears/",
+
+	image="div.blog-content>img.size-full",
+	next="h3.next-post-title>a",
+),
+
+
 # Sarjakuva(
 # 	"nimi", 
 # 	"alias",
